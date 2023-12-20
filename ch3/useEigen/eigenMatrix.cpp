9f717c1048ffd88f7c4975806f7f283918186750
lizishuo02 <lizishuo02@baidu.com>
2023-12-20 16:23:06 +0800

    ch3 3.2 practices

--- ch3/useEigen/eigenMatrix.cpp
+++ ch3/useEigen/eigenMatrix.cpp
@@ -20,7 +20,8 @@ int main(int argc, char **argv) {
   // Eigen 中所有向量和矩阵都是Eigen::Matrix，它是一个模板类。它的前三个参数为：数据类型，行，列
   // 声明一个2*3的float矩阵
   Matrix<float, 2, 3> matrix_23;
-
+  // lizishuo02 local test
+  cout << "lizishuo02 local test" << endl;
   // 同时，Eigen 通过 typedef 提供了许多内置类型，不过底层仍是Eigen::Matrix
   // 例如 Vector3d 实质上是 Eigen::Matrix<double, 3, 1>，即三维向量
   Vector3d v_3d;
